99829a62a4893075c1604675166d071e168aa47d
Fateh Singh <fatehsingh.sunny5@gmail.com>
2018-03-31 11:01:35 +0530

    new changes

--- program_1.c
+++ program_1.c
@@ -1,45 +1,55 @@
 #include <stdio.h>
 #include <math.h>
-int max(int num1, int num2);
+#define PI 3.141593
+float convert_d_to_r(int angleDegrees);
+float convert_r_to_d(float angleRadians);
 int main() {
-  int number;
-  int d_to_r;
-  float r_to_d;
-  //  student number, student name and the assignment number
-  printf(" 7777777  Fateh Singh 1\n");
-  max(1,2);
+  int   number;
+  int   degree;
+  float radians;
+  float result;
+  // student number, student name and the assignment number
+  printf(" 7777777  Fateh Singh 1 \n");
   printf("Please select the table you would like to create \n");
-  ineligible:
+  try_again:
   printf("1. Degrees to Radians or 2. Radians to Degrees : " );
   scanf("%d", &number);
 
-  switch (number){
-      case 1:
-          printf("Please enter the step size of the table (1-359): " );
-          scanf("%d", &d_to_r);
-          break;
-      case 2:
-          printf("Please enter the step size of the table (0.01 - 6.26): " );
-          scanf("%d", &r_to_d);
-          break;
-      default:
-          printf("Invalid input, please try again. \n" );
-          goto ineligible;
-  }
+  switch(number){
 
+    case 1:
+      printf("Please enter the step size of the table (1-359): " );
+      scanf("%d", &degree);
+      result = convert_d_to_r(degree);
+      printf("\n %d degree | %.2f radians\n", degree, result);
+      printf("\n");
+      break;
+
+    case 2:
+      printf("Please enter the step size of the table (0.01 - 6.26): " );
+      scanf("%f", &radians);
+      result = convert_r_to_d(radians);
+      printf("\n %.2f radians | %.2f degree\n", radians, result);
+      printf("\n");
+      break;
+
+    default:
+      printf("Invalid input, please try again. \n" );
+      goto try_again;
+
+  }
    return 0;
 }
+// Degrees to Radians
+float convert_d_to_r(int angleDegrees) {
+  float result ;
+  result = ((angleDegrees)*(PI / 180.0));
+  return result;
+}
 
-/* function returning the max between two numbers */
-int max(int num1 , int num2) {
-
-   /* local variable declaration */
-   int result;
-
-   if (num1 > num2)
-      result = num1;
-   else
-      result = num2;
-      printf("%d\n",result );
-   return result;
+// Radians to Degrees
+float convert_r_to_d(float angleRadians){
+  float result ;
+  result = ((angleRadians) * (180.0 / PI) );
+  return result;
 }
